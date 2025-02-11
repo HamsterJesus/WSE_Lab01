121706cfc5bbc73427bbc12b786c114f04886e42
HamsterJesus <HamsterJesus8@gmail.com>
2025-02-11 14:40:20 +0000

    changes to sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ npm install
 sudo ufw allow 8080/tcp
 npm install mongodb
 cd backend
-uri="mongodb://testUser:goblinson@127.0.0.1:27017" node server.js
+uri="mongodb://testUser:goblinson@127.0.0.1:27017/mydb" node server.js
